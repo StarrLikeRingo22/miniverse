734aef275259da4d3d998abe7558a16af68a20f4
Abdalla <aabdelgadir@myseneca.ca>
2024-12-26 16:19:48 -0500

    changed main.jsx placement and script tag

--- public/main.jsx
+++ public/main.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import Scene from "./Scene"
+import Scene from "../src/Scene"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(<Scene />)
