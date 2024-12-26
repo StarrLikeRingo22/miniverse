b4046d844b707c0665b37421d5505a4ea131839a
Abdalla <aabdelgadir@myseneca.ca>
2024-12-26 12:01:59 -0500

    three link

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // This specifies where the built assets should go
+    outDir: 'public', // Set this to 'public' so the build assets are placed there
+    assetsDir: 'assets', // Optionally specify assets directory for images or other static files
+    rollupOptions: {
+      input: {
+        main: '/index.html',  // Main entry point
+        three: '/Three.html'  // Your custom entry for Three.js experience
+      },
+    },
+  },
 })
