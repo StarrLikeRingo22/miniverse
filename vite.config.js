9976af7041bdb8fd60601717cadab160f0ccdfaa
Abdalla <aabdelgadir@myseneca.ca>
2024-12-26 12:07:33 -0500

    configuring three link

--- vite.config.js
+++ vite.config.js
@@ -8,11 +8,5 @@ export default defineConfig({
     // This specifies where the built assets should go
     outDir: 'public', // Set this to 'public' so the build assets are placed there
     assetsDir: 'assets', // Optionally specify assets directory for images or other static files
-    rollupOptions: {
-      input: {
-        main: '/index.html',  // Main entry point
-        three: '/Three.html'  // Your custom entry for Three.js experience
-      },
-    },
   },
 })
