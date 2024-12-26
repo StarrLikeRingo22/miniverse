4ed44a6a81cf0f247faab84ccb157810449763b2
Abdalla <aabdelgadir@myseneca.ca>
2024-12-26 17:19:27 -0500

    added vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist', // Ensure the output directory is set to 'dist'
-  }
-})
+    rollupOptions: {
+      input: './src/main.js',  // Point to your main entry file
+    },
+  },
+});
