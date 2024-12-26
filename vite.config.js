3194f661e01be6cbdf5c4ba25f010490a4b72e33
Abdalla <aabdelgadir@myseneca.ca>
2024-12-26 12:17:49 -0500

    fixed error

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // This specifies where the built assets should go
-    outDir: 'public', // Set this to 'public' so the build assets are placed there
-    assetsDir: 'assets', // Optionally specify assets directory for images or other static files
-  },
 })
