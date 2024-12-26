0eee1b80acbfadaa63ab4455a511b552c510ad52
Abdalla <aabdelgadir@myseneca.ca>
2024-12-26 17:34:40 -0500

    changed vercel config

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,5 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist', // Ensure the output directory is set to 'dist'
-    rollupOptions: {
-      input: './src/main.jsx',  // Point to your main entry file
-    },
   },
 });
