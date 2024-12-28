26bfdf5a3588115a7e21fbc3fe3e041e4563fdc8
Abdalla <aabdelgadir@myseneca.ca>
2024-12-28 09:26:33 -0500

    phone fov

--- src/components/Mobile.jsx
+++ src/components/Mobile.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Planecard from "./Planecard";
+import { useThree } from "@react-three/fiber";
 
 
 
 const Mobile = ({ handleCardClick, activeCard, setActiveCard }) => {
   const cardSize = [0.5, 0.7, 0.5] // Adjusted size for mobile
+
+
+
+  const { camera } = useThree();
+
+  useEffect(() => {
+    // Adjust the camera for mobile view
+    camera.fov = 30; // Narrower field of view for mobile
+    camera.position.set(0, 0, 5); // Position the camera to center the mobile layout
+    camera.updateProjectionMatrix(); // Apply the changes to the camera
+  }, [camera]);
+
+
   return (
     <>
       <Planecard
@@ -16,7 +30,7 @@ const Mobile = ({ handleCardClick, activeCard, setActiveCard }) => {
         setActiveCard={setActiveCard}
       />
       <Planecard
-        position={[0, -2, 0]}
+        position={[-2, 0, 0]}
         args={cardSize}
         onClick={(position) => handleCardClick(position, 1)}
         cardId={1}
@@ -24,7 +38,7 @@ const Mobile = ({ handleCardClick, activeCard, setActiveCard }) => {
         setActiveCard={setActiveCard}
       />
       <Planecard
-        position={[0, -4, 0]}
+        position={[-4, 0, 0]}
         args={cardSize}
         onClick={(position) => handleCardClick(position, 2)}
         cardId={2}
@@ -32,7 +46,7 @@ const Mobile = ({ handleCardClick, activeCard, setActiveCard }) => {
         setActiveCard={setActiveCard}
       />
       <Planecard
-        position={[0, -6, 0]}
+        position={[-6, 0, 0]}
         args={cardSize}
         onClick={(position) => handleCardClick(position, 3)}
         cardId={3}
